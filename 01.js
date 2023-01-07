670b3267fd80fb853b55f512103cce7b6cba3aa0
Loa-ok <marilyndurand2022@gmail.com>
2023-01-07 18:47:59 -0300

    resolucionsimulacionej1

--- 01.js
+++ 01.js
@@ -8,11 +8,19 @@ function soloNumeros(array) {
   // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
   // Debe devolver un arreglo con solo los enteros.
   // Ej: 
-  // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
+  // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2[
 
   // Tu código aca:
+    var soloenteros = [];
 
-}
+    for (var i = 0; i < array.length; i++) {
+      if (typeof(array[i])=="number") { 
+         soloenteros.push(array[i]);
+      }; 
+    }
+   return soloenteros;
+  }  
+  
 
 // No modifiques nada debajo de esta linea //
 
