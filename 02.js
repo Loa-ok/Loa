77f4be077b2c532d09facd5c74fb47b4664d2672
Loa-ok <marilyndurand2022@gmail.com>
2023-01-07 17:47:24 -0300

    resolucionsimulacionej2

--- 02.js
+++ 02.js
@@ -12,6 +12,13 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  let stringgrande = [];
+
+  for (let i = 0; i < strings.length; i++) {
+    if (strings [i] > stringgrande)
+    stringgrande = strings [i];
+  }
+  return stringgrande;
 }
 
 // No modifiques nada debajo de esta linea //
