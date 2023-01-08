acde6aa51a78fcc70a8d8b51ad3e864effc1bfdd
Loa-ok <marilyndurand2022@gmail.com>
2023-01-08 16:48:00 -0300

    resolucionsimulacionej09

--- 09.js
+++ 09.js
@@ -25,6 +25,16 @@ function filtrar(funcion) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
 
+  Array.prototype.filtrar = function (funcion) {
+    let result = []
+    for (let i = 0; i < this.length; i++) {
+      if (funcion(this[i])) {
+        result.push(this[i])
+      } 
+    }
+    return result;
+  };
+
 };
 
 // No modifiques nada debajo de esta linea //
